062105f6f28f563c1f614d6a3aa9ec261ba7fdd3
Miguel Cabrera <miguelcabsan@gmail.com>
2014-10-01 13:25:14 +0100

    programa finalizado, falta pruebas

--- Actividad 5/script.js
+++ Actividad 5/script.js
@@ -1,3 +1,4 @@
+//Funciones
 function buscarAlumno(clase, nombre) {
     for (var i = 0; i < clase.length; i++)
         if (clase[i][0].toUpperCase == nombre.toUpperCase)
@@ -16,6 +17,7 @@ function modificarNota(clase) {
             else
                 correcto = true;
         clase[alumnoBuscado][1] = notaNueva;
+        alert("Nota modificada");
     }
     else
         alert("No existe alumno con ese nombre.");
@@ -41,6 +43,7 @@ function obtenerPeorNota(clase) {
             candidato = clase[i];
     return candidato;
 }
+//Creacion del array
 var clase = new Array();
 for (var i = 0; i < 10; i++) {
     for (var j = 0; j < 2; j++) {
@@ -61,5 +64,37 @@ for (var i = 0; i < 10; i++) {
                 clase[i][j] = variable;
                 break;
         }
+    }
+}
+//Menu
+var salir = false;
+while(!salir) {
+    var correcto = false;
+    while(!correcto) {
+        var opcion = parseInt(prompt("MENÚ:\n[1]Buscar alumno.\n[2]Modificar nota de alumno.\n[3]Obtener media\n[4]Obtener el alumno con más nota.\n[5]Obtener el alumno con menos nota.\nElija una opción:"));
+        if(isNaN(opcion))
+            alert("La opción introducida no es numérica");
+        else
+            correcto = true;
+    }
+    switch(opcion) {
+        case 1: var nombre = prompt("Escriba el nombre del alumno:");
+            var alumnoBuscado = buscarAlumno(clase, nombre);
+            if(alumnoBuscado != -1)
+                alert("Alumno - " + clase[alumnoBuscado][0] + "\nNota: " + clase[alumnoBuscado][1]);
+            else
+                alert("No existe un alumno con ese nombre.")
+            break;
+        case 2: modificarNota(clase);
+            break;
+        case 3: var media = obtenerMedia(clase);
+            alert("Media de la clase: " + media);
+            break;
+        case 4: var alumnoMejorNota = obtenerMejorNota(clase);
+            alert("Alumno con mejor nota.\nNombre: " + alumnoMejorNota[0] + "\nNota:" + alumnoMejorNota[1]);
+            break;
+        case 5: var alumnoPeorNota = obtenerPeorNota(clase);
+            alert("Alumno con peor nota.\nNombre: " + alumnoPeorNota[0] + "\nNota:" + alumnoPeorNota[1]);
+            break;
     }
 }
